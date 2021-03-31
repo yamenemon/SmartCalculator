094ad2e56f43a5d2b43f95e6f1a5232b77459ed2
Yamen Emon <khaliful.emon@sslwireless.com>
2021-03-31 10:59:53 +0600

    Init commit

---
+++ SmartCalculator/SmartCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartCalculator
+//
+//  Created by Yamen Emon on 31/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
